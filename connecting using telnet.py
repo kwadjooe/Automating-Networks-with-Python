d8661ae50eeb4fc0c2b2801a0380352ecd9a98c8
Kwadjo <kwadjooe@gmail.com>
2021-08-26 14:46:29 -0400

    fix minor typos

--- connecting using telnet.py
+++ connecting using telnet.py
@@ -1,18 +1,18 @@
 import getpass
 import telnetlib
 
-IP = input("Enter the IP Address: ")
-user: input("Enter your Username: ")
-password = getpass.getpass
+IP= input("Enter the IP Address: ")
+user= input("Enter your Username: ")
+password= getpass.getpass
 tn = telnetlib.Telnet(IP)
 
 
 tn.read_until(b"Username: ")
-tn.write(user.encode("ascii") + b"\n")
+tn.write(user.encode('ascii') + b"\n")
 
 if password:
     tn.read_until(b"Password: ")
-    tn.write(password.encode("ascii") + b"\n")
+    tn.write(password.encode('ascii') + b"\n")
 
 tn.write(b"enable\n")
 tn.write(b"cisco\n")
