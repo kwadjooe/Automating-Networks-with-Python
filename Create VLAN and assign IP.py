e0cb059aac9d0f34d9d23b376ed894e5d5e3fc1e
Kwadjo <kwadjooe@gmail.com>
2021-08-28 10:24:41 -0400

    added two more file for configuring vlan and getting the running config

---
+++ Create VLAN and assign IP.py
@@ -0,0 +1,29 @@
+import telnetlib
+import getpass
+
+
+IP = input("Enter the IP Address: ")
+user = input("Enter your Username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(IP)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.write(password.encode(ascii) + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"conf t\n")
+tn.write(b"vlan 10\n")
+tn.write(b"name LAB\n")
+tn.write(b"exit\n")
+tn.write(b"int vlan 10\n")
+tn.write(b"ip add 10.1.10.1 255.255.255.0\n")
+tn.write(b"no shut\n")
+tn.write(b"end\n")
+tn.write(b"sh ip int brief\n")
+tn.write(b'exit\n')
+
+print(tn.read_all().decode('ascii'))
---
+++ connecting using telnet to get running config.py
@@ -0,0 +1,24 @@
+import getpass
+import telnetlib
+
+IP= input("Enter the IP Address: ")
+user= input("Enter your Username: ")
+password= getpass.getpass()
+tn = telnetlib.Telnet(IP)
+
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"show run\n")
+tn.write(b"\n")
+tn.write(b"exit\n")
+
+
+print(tn.read_all().decode('ascii'))
