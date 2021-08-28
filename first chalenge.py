380061c47be1ba9ed5a3ba62245a199eae6e6221
Kwadjo <kwadjooe@gmail.com>
2021-08-28 11:01:18 -0400

    added first challenge: delete vlan,create loopback iface and assign IP,create new user

---
+++ first chalenge.py
@@ -0,0 +1,28 @@
+import telnetlib
+import getpass
+
+
+IP = input("Enter the IP Address: ")
+user = input("Enter your Username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(IP)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"conf t\n")
+tn.write(b"no vlan 10\n")
+tn.write(b"int lo2\n")
+tn.write(b"ip add 2.2.2.2 255.255.255.255\n")
+tn.write(b"exit\n")
+tn.write(b"username admin secret cisco\n")
+tn.write(b'sh run \n')
+tn.write(b"\n")
+tn.write(b"exit")
+
+print(tn.read_all().decode('ascii'))
